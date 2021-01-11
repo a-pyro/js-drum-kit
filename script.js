a8e15c2c7d6ef3bc0b509eb1e62325ea9e736c11
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-01-11 17:20:40 +0100

    add sound

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
 console.log('Hello There');
+const playSound = e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`section[data-key="${e.keyCode}"]`);
+    console.log(key);
+    audio.play();
+};
+
+window.addEventListener('keydown', playSound);
