ed88ef4441141f52f940584285b3566a4332e735
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-01-11 18:45:52 +0100

    add rec play btn styles

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 'use strict';
 console.log('Hello There');
+// record
+let recording = false;
+// add memory
+const playedKeys = [];
+const rec = document.querySelector('#rec');
+const play = document.querySelector('#play');
+
+rec.addEventListener('click', function () {
+    if (!recording) {
+        recording = true;
+        this.classList.add('recording');
+    } else {
+        recording = false;
+        this.classList.remove('recording');
+    }
+
+    // console.log(recording);
+});
+
 const playSound = e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`section[data-key="${e.keyCode}"]`);
