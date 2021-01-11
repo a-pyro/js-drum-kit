9a5ba94ed05576e4216dd58790e41f8384822467
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-01-11 17:35:51 +0100

    add javascript

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ console.log('Hello There');
 const playSound = e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`section[data-key="${e.keyCode}"]`);
-    console.log(key);
+
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
     audio.play();
 };
 
+const keys = Array.from(document.querySelectorAll('.key'));
+
+const removeTransion = e => {
+    e.target.classList.remove('playing');
+};
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransion));
 window.addEventListener('keydown', playSound);
